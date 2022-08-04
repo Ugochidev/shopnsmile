2510f7dd581504511712cdf3c3263a1c8f305382
Ugochidev <ugochukwuchioma16@gmail.com>
2022-08-04 06:53:20 +0100

    feat/modified

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,46 @@
-const Product = require ("../models/product.model")
+const Product = require("../models/product.model");
 
+const addProduct = async (req, res, next) => {
+  try {
+    const {
+      categories,
+      productName,
+      brandName,
+      manufacturingDate,
+      expiryDate,
+      quantityAvailable,
+      pricePerItem,
+    } = req.body;
+    const newProduct = new Product({
+      productId: uuid.v4(),
+      categories,
+      productName,
+      brandName,
+      manufacturingDate,
+      expiryDate,
+      quantityAvailable,
+      pricePerItem,
+      dateCreated: newProduct.createdAt,
+    });
+    await newProduct.save;
+    return res.status(201).json({ message: "Product added successfully." });
+  } catch (error) {
+    return res.status(500).json({
+      messsage: error.message,
+    });
+  }
+};
 
-
-const addProduct = async (req, res, next) =>{
-    try {
-        const {} = req.body
-    } catch (error) {
-        
-    }
-}
+const fetchAllProduct = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
+module.exports = {
+  addProduct,
+  fetchAllProduct,
+  fetchSingleProduct,
+  fetchProductByCategory,
+  fetchProductByname,
+  updateProduct,
+  deleteProduct,
+};
